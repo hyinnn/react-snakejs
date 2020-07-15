63c6fc7af802795d845da232350674ba6ead9a19
hyinnn <henry.yin@uq.net.au>
2020-07-15 11:22:28 +1000

    Updates

---
+++ precache-manifest.53139626d3fb5a743d7c6d0b97946563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49bcf52134d1a5d528ffab762987f11f",
+    "url": "/react-snakejs/index.html"
+  },
+  {
+    "revision": "858c48e54d3774408562",
+    "url": "/react-snakejs/static/css/main.8e9b9d16.chunk.css"
+  },
+  {
+    "revision": "071afa829f9f15ab5727",
+    "url": "/react-snakejs/static/js/2.419882d7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-snakejs/static/js/2.419882d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "858c48e54d3774408562",
+    "url": "/react-snakejs/static/js/main.75ab9df1.chunk.js"
+  },
+  {
+    "revision": "04205719df9e58b0b73b",
+    "url": "/react-snakejs/static/js/runtime-main.8c589c75.js"
+  }
+]);
